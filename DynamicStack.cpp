64ec8f3e365f878fc2236cfe5e0998d641222991
Tayyaba463 <tayyabakhan773@gmail.com>
2026-02-25 12:16:03 +0500

    Update DynamicStack.cpp

--- DynamicStack.cpp
+++ DynamicStack.cpp
@@ -1,75 +1,88 @@
 #include <iostream>
-#include <stdlib.h>
 using namespace std;
 
-struct Node {
-    int data;
-    Node* next;
+struct Node
+{
+   int data;
+   Node* next;
 };
 
 Node* top = NULL;
 
-
-void push(int value) {
-    Node* temp = (Node*)malloc(sizeof(Node));
+void insertData (int value) 
+{ 
+    Node* temp = (Node*) malloc (sizeof(Node));
+   
     temp->data = value;
     temp->next = top;
     top = temp;
 }
 
-
-void pop() {
-    if (top == NULL) {
-        cout << "Stack Underflow" << endl;
+void popData ( ) 
+ {
+     if (top == NULL) {
+        cout << "Stack is Empty." << endl;
         return;
     }
-    Node* temp = top;
-    top = top->next;
-    free(temp);
-}
 
-
-void display() {
-    if (top == NULL) {
-        cout << "Stack is Empty" << endl;
-        return;
-    }
-    Node* curr = top;
-    while (curr != NULL) {
-        cout << curr->data << " ";
-        curr = curr->next;
-    }
-    cout << endl;
-}
-
-int main() {
-
- int num, value;
-
-    while (true) {
-   
-        cout << "1. Push" << endl;
-        cout << "2. Pop" << endl;
-        cout << "3. Display" << endl;
-        cout << "4. Exit" << endl;
-        cin >> num;
-
- if (num == 1){
-    cout << "Enter value: ";
-        cin >> value;
-         push(value); 
-        }
-   else if (num == 2) {
-        pop();
-        cout << "Value has been successfully poped(deleted)." << endl;
-    }
-     else if (num == 3) {
-        display();
-     }       
-
-    else if (num == 4) {
-        cout << "Exited Successfully." << endl;
-    }
+    Node* current = top;
+       top = top->next;
+      free(current);
+      cout << "Value has been successfully been poped." << endl;
  }
-}
-   
+
+ void display ()
+ {
+     if (top == NULL) {
+        cout << "Stack is Empty." << endl;
+        return;
+    }
+
+    Node* current = top; 
+     while (current != NULL)
+      {
+         cout << current->data << " ";
+         current = current->next;
+      }
+      cout << endl;
+ }
+
+ int main()
+ { 
+    int number, value;
+
+     while (true)
+     {
+        cout << "1.Push Value." << endl;
+        cout << "2.Pop Value." << endl;
+        cout << "3.Display All." << endl;
+        cout << "4.Exit." << endl;
+
+        cin >> number;
+
+        if (number == 1)
+        {
+          cout << "Top the value.";
+          cin >> value;
+          insertData(value);
+        }
+        else if (number == 2)
+        {
+            popData();
+        }
+       else  if (number == 3)
+        {
+            display();
+        }
+        else if (number == 4)
+        {
+            cout << "Exit Successfully from the system." << endl;
+            break;
+        }
+        else 
+        {
+            cout << "Enter Correct Choice." << endl;
+        }
+     }
+      return 0;
+ }
